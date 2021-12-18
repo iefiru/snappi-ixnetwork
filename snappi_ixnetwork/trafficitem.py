c8fd849c38d72e2d27c5d95b6277f5ea39e1ff9c
chestercheng <chester7864@gmail.com>
2021-12-18 21:14:36 +0800

    test: adding test to validates vxlan fields

--- snappi_ixnetwork/trafficitem.py
+++ snappi_ixnetwork/trafficitem.py
@@ -306,6 +306,7 @@ class TrafficItem(CustomField):
         "vni": "vxlan.header.vni",
         "reserved1": "vxlan.header.reserved8",
         "order": ["flags", "reserved0", "vni", "reserved1"],
+        "convert_int_to_hex": ["flags"],
     }
 
     def __init__(self, ixnetworkapi):
